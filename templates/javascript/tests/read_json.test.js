955dc5cc28dba63ff58088cb2c548734cd6fd0e4
Joe Reddington <joe@joereddington.com>
2018-10-22 10:37:32 +0100

    I've dubplicated all the tests we use for OVF internal format to test the OBF internal format.  Going to be working on the add command

--- templates/javascript/tests/read_json.test.js
+++ templates/javascript/tests/read_json.test.js
@@ -2,7 +2,7 @@
  * Unit tests for javascript/comscan.js
  *
  */
-describe('Comscan', function() {
+describe('Open Voice Factory internal format behaviour tests', function() {
 
     // inject the HTML fixture for the tests
     beforeEach(function() {
@@ -10,146 +10,214 @@ describe('Comscan', function() {
         document.body.insertAdjacentHTML(
             'afterbegin',
             fixture);
-        setupInternalDataStructures(sample);	
-	setupMessageWindow();
+        setupInternalDataStructures(sample);
+        setupMessageWindow();
 
     });
 
 
-// * Does that object have the right number of slides in it. 
+    // * Does that object have the right number of slides in it. 
     it('We can parse an example json document', function() {
         expect(Object.keys(links).length).toBe(100);
     });
 
-// * When loading the first page. Do a selection of the right squares have the correct message in them?
-    it('When loading top page, the utterances are in the right place', function() {
-        expect(labels["toppage"][0][0]).toBe('Yes');
-        expect(labels["toppage"][1][1]).toBe('I want to talk to you.');
-    });
 
-// * When pressing a particular button, does the message window change apropreityly 
-  it('Message window sets up and changes with button press.', function() {
-	expect(document.getElementById("messagewindow").value).toBe("");
-	add(1,1);
-	expect(document.getElementById("messagewindow").value).toBe(" I want to talk to you.");
+    // * When pressing a particular button, does the message window change apropreityly 
+    it('Message window sets up and changes with button press.', function() {
+        expect(document.getElementById("messagewindow").value).toBe("");
+        add(1, 1);
+        expect(document.getElementById("messagewindow").value).toBe(" I want to talk to you.");
     });
 
 
- it('clear function works', function() {
-	expect(document.getElementById("messagewindow").value).toBe("");
-	add(1,1);
-	expect(document.getElementById("messagewindow").value).toBe(" I want to talk to you.");
-	add(0,4);
-	expect(document.getElementById("messagewindow").value).toBe("");
+    it('clear function works', function() {
+        expect(document.getElementById("messagewindow").value).toBe("");
+        add(1, 1);
+        expect(document.getElementById("messagewindow").value).toBe(" I want to talk to you.");
+        add(0, 4);
+        expect(document.getElementById("messagewindow").value).toBe("");
     });
 
 
-   it('clear function works', function() {
-	expect(document.getElementById("messagewindow").value).toBe("");
-	add(1,1);
-	expect(document.getElementById("messagewindow").value).toBe(" I want to talk to you.");
-	add(0,4);
-	expect(document.getElementById("messagewindow").value).toBe("");
+    it('clear function works', function() {
+        expect(document.getElementById("messagewindow").value).toBe("");
+        add(1, 1);
+        expect(document.getElementById("messagewindow").value).toBe(" I want to talk to you.");
+        add(0, 4);
+        expect(document.getElementById("messagewindow").value).toBe("");
     });
 
 
-  it('Two part function works', function() {
-	expect(document.getElementById("messagewindow").value).toBe("");
-	add(2,2);
-	add(3,1);
-	add(1,1);
-	expect(document.getElementById("messagewindow").value).toBe(" I have a pain in my");
-	expect(key).toBe("bodyparts")
+    it('Two part function works', function() {
+        expect(document.getElementById("messagewindow").value).toBe("");
+        add(2, 2);
+        add(3, 1);
+        add(1, 1);
+        expect(document.getElementById("messagewindow").value).toBe(" I have a pain in my");
+        expect(key).toBe("bodyparts")
 
 
     });
 
-// * When pressing a particular button, does the contents of other buttons change. 
-  it('the add function changes the keys', function() {
-        setupInternalDataStructures(sample);	
-	add(2,2);
-	expect(key).toBe("personalcare");
+    // * When pressing a particular button, does the contents of other buttons change. 
+    it('the add function changes the keys', function() {
+        setupInternalDataStructures(sample);
+        add(2, 2);
+        expect(key).toBe("personalcare");
+    });
+});
+
+
+describe("Manifest testing", function() {
+    beforeEach(function(done) {
+        // You can call any async task, when done() is called the test will begin
+        manifest = ""
+        load_json_file("manifest.json", function hope() {;
+        });
+        setTimeout(() => {
+            done();
+        }, 1000);
     });
 
 
-	describe("Manifest testing", function() {
-		beforeEach(function(done){
-			// You can call any async task, when done() is called the test will begin
-			manifest=""
-			load_json_file("manifest.json", function hope(){;});
-			setTimeout(() => {done();}, 1000); 
-		}); 
-		
-		
-		it('We can get a manifest file', function(done) {
-			expect(manifest.length).not.toBeLessThan(1)
-			setTimeout(done(),2000)
-			});			
-
-		
-		it('Check manifest file reads correctly', function(done) {
-			expect(manifest['format']).toBe("open-board-0.1")
-			setTimeout(done(),2000)
-			});			
-			});	
+    it('We can get a manifest file', function(done) {
+        expect(manifest.length).not.toBeLessThan(1)
+        setTimeout(done(), 2000)
+    });
 
 
-	describe("Load Simple OBF file", function() {
-		beforeEach(function(done){
-			// You can call any async task, when done() is called the test will begin
-			currentpage=""
-			manifest=""
-			load_obf_page("boards/toppage.obf");
-			setTimeout(() => {done();}, 1000); 
-		}); 
-		
-		
-		it('Mainpageloaded', function(done) {
-			expect(currentpage.length).not.toBeLessThan(1)
-			done()
-			});			
-
-		
-		it('Check the number of columns', function(done) {
-			expect(currentpage.grid.columns).toBe(5)
-			done()
-			});			
-
-		
-		it('Work out the grid', function(done) {
-			expect(obfadd(3,3).label).toBe("Describing")
-			done()
-			});			
-});	
-
-	describe("Load root file from manifest", function() {
-		beforeEach(function(done){
-			currentpage=""
-			manifest=""
-			// You can call any async task, when done() is called the test will begin
-			load_json_file("manifest.json", parse_manifest);
-			setTimeout(() => {done();}, 1000); 
-		}); 
-		
-		it('Work out the grid', function(done) {
-			expect(obfadd(3,3).label).toBe("Describing")
-			done()
-			});			
-});	
+    it('Check manifest file reads correctly', function(done) {
+        expect(manifest['format']).toBe("open-board-0.1")
+        setTimeout(done(), 2000)
+    });
+});
 
 
+describe("Load Simple OBF file", function() {
+    beforeEach(function(done) {
+        // You can call any async task, when done() is called the test will begin
+        currentpage = ""
+        manifest = ""
+        load_obf_page("boards/toppage.obf");
+        setTimeout(() => {
+            done();
+        }, 1000);
+    });
 
 
-	
+    it('Mainpageloaded', function(done) {
+        expect(currentpage.length).not.toBeLessThan(1)
+        done()
+    });
 
-//Keep for the spy snippet 
-//it('Speak function is triggered...', function() {
- //       spyOn(window, "say");
-  //      expect(think).toHaveBeenCalled();
-   //     //learned this at http://www.htmlgoodies.com/html5/javascript/spy-on-javascript-methods-using-the-jasmine-testing-framework.html#fbid=KJtVgELupgs
-  //  });
 
+    it('Check the number of columns', function(done) {
+        expect(currentpage.grid.columns).toBe(5)
+        done()
+    });
+
+
+    it('Work out the grid', function(done) {
+        expect(obfadd(3, 3).label).toBe("Describing")
+        done()
+    });
+});
+
+describe("Load root file from manifest", function() {
+    beforeEach(function(done) {
+        currentpage = ""
+        manifest = ""
+        // You can call any async task, when done() is called the test will begin
+        load_json_file("manifest.json", parse_manifest);
+        setTimeout(() => {
+            done();
+        }, 1000);
+    });
+
+    it('Work out the grid', function(done) {
+        expect(obfadd(3, 3).label).toBe("Describing")
+        done()
+    });
 });
 
 
 
+describe('Open Board Format behaviour tests ', function() {
+
+    // inject the HTML fixture for the tests
+    beforeEach(function() {
+        var fixture = '<table id="mainGrid" border=1 height=520 width=720 class="four"></table> <form name="myform"> <textarea id="messagewindow" name="outputtext"></textarea> </form>';
+        document.body.insertAdjacentHTML(
+            'afterbegin',
+            fixture);
+        currentpage = ""
+        manifest = ""
+        // You can call any async task, when done() is called the test will begin
+        load_json_file("manifest.json", parse_manifest);
+        setTimeout(() => {
+            done();
+        }, 1000);
+
+    });
+
+
+    // * Does that object have the right number of slides in it. 
+    it('We can parse an example json document', function() {
+        expect(Object.keys(links).length).toBe(100);
+    });
+
+
+    // * When pressing a particular button, does the message window change apropreityly 
+    it('Message window sets up and changes with button press.', function() {
+        expect(document.getElementById("messagewindow").value).toBe("");
+        add(1, 1);
+        expect(document.getElementById("messagewindow").value).toBe(" I want to talk to you.");
+    });
+
+
+    it('clear function works', function() {
+        expect(document.getElementById("messagewindow").value).toBe("");
+        add(1, 1);
+        expect(document.getElementById("messagewindow").value).toBe(" I want to talk to you.");
+        add(0, 4);
+        expect(document.getElementById("messagewindow").value).toBe("");
+    });
+
+
+    it('clear function works', function() {
+        expect(document.getElementById("messagewindow").value).toBe("");
+        add(1, 1);
+        expect(document.getElementById("messagewindow").value).toBe(" I want to talk to you.");
+        add(0, 4);
+        expect(document.getElementById("messagewindow").value).toBe("");
+    });
+
+
+    it('Two part function works', function() {
+        expect(document.getElementById("messagewindow").value).toBe("");
+        add(2, 2);
+        add(3, 1);
+        add(1, 1);
+        expect(document.getElementById("messagewindow").value).toBe(" I have a pain in my");
+        expect(key).toBe("bodyparts")
+
+
+    });
+
+    // * When pressing a particular button, does the contents of other buttons change. 
+    it('the add function changes the keys', function() {
+        setupInternalDataStructures(sample);
+        add(2, 2);
+        expect(key).toBe("personalcare");
+    });
+});
+
+
+
+
+//Keep for the spy snippet 
+//it('Speak function is triggered...', function() {
+//       spyOn(window, "say");
+//      expect(think).toHaveBeenCalled();
+//     //learned this at http://www.htmlgoodies.com/html5/javascript/spy-on-javascript-methods-using-the-jasmine-testing-framework.html#fbid=KJtVgELupgs
+//  });
